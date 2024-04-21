6474bb8e7440bbff6289234442c04b1ac983d0de
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-04-21 15:32:58 +0000

    Updating to latest FlutterFlow output.

--- lib/components/booking_action_widget.dart
+++ lib/components/booking_action_widget.dart
@@ -47,70 +47,88 @@ class _BookingActionWidgetState extends State<BookingActionWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsetsDirectional.fromSTEB(20.0, 0.0, 20.0, 0.0),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Icon(
-            Icons.menu_book,
-            color: FlutterFlowTheme.of(context).secondaryText,
-            size: 44.0,
-          ),
-          Text(
-            'Boo your Consultation',
-            style: FlutterFlowTheme.of(context).titleLarge.override(
-                  fontFamily: 'Outfit',
-                  letterSpacing: 0.0,
+    return Container(
+      height: double.infinity,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          fit: BoxFit.cover,
+          image: Image.network(
+            'https://images.unsplash.com/photo-1511208687438-2c5a5abb810c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NTYyMDF8MHwxfHNlYXJjaHwxfHx3aWxkfGVufDB8fHx8MTcxMzcxMTU4N3ww&ixlib=rb-4.0.3&q=80&w=1080',
+          ).image,
+        ),
+        borderRadius: const BorderRadius.only(
+          bottomLeft: Radius.circular(0.0),
+          bottomRight: Radius.circular(0.0),
+          topLeft: Radius.circular(20.0),
+          topRight: Radius.circular(20.0),
+        ),
+      ),
+      child: Padding(
+        padding: const EdgeInsetsDirectional.fromSTEB(20.0, 0.0, 20.0, 0.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.menu_book,
+              color: FlutterFlowTheme.of(context).secondaryText,
+              size: 44.0,
+            ),
+            Text(
+              'Book your consultation',
+              style: FlutterFlowTheme.of(context).titleLarge.override(
+                    fontFamily: 'Outfit',
+                    letterSpacing: 0.0,
+                  ),
+            ),
+            Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Book with ${widget.user} at ${dateTimeFormat('M/d h:mm a', widget.time)}',
+                  style: FlutterFlowTheme.of(context).bodySmall.override(
+                        fontFamily: 'Readex Pro',
+                        letterSpacing: 0.0,
+                      ),
                 ),
-          ),
-          Row(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Book with ${widget.user} at ${dateTimeFormat('M/d h:mm a', widget.time)}',
-                style: FlutterFlowTheme.of(context).bodySmall.override(
-                      fontFamily: 'Readex Pro',
-                      letterSpacing: 0.0,
-                    ),
-              ),
-            ],
-          ),
-          Padding(
-            padding: const EdgeInsetsDirectional.fromSTEB(0.0, 10.0, 0.0, 20.0),
-            child: FFButtonWidget(
-              onPressed: () async {
-                await BookingsRecord.createDoc(widget.userid!)
-                    .set(createBookingsRecordData(
-                  time: widget.time,
-                  uid: currentUserReference,
-                ));
-                Navigator.pop(context);
-              },
-              text: 'Book',
-              options: FFButtonOptions(
-                width: double.infinity,
-                height: 40.0,
-                padding: const EdgeInsetsDirectional.fromSTEB(24.0, 0.0, 24.0, 0.0),
-                iconPadding: const EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 0.0, 0.0),
-                color: FlutterFlowTheme.of(context).primary,
-                textStyle: FlutterFlowTheme.of(context).titleSmall.override(
-                      fontFamily: 'Readex Pro',
-                      color: Colors.white,
-                      letterSpacing: 0.0,
-                    ),
-                elevation: 3.0,
-                borderSide: const BorderSide(
-                  color: Colors.transparent,
-                  width: 1.0,
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsetsDirectional.fromSTEB(0.0, 10.0, 0.0, 20.0),
+              child: FFButtonWidget(
+                onPressed: () async {
+                  await BookingsRecord.createDoc(widget.userid!)
+                      .set(createBookingsRecordData(
+                    time: widget.time,
+                    uid: currentUserReference,
+                  ));
+                  Navigator.pop(context);
+                },
+                text: 'Book',
+                options: FFButtonOptions(
+                  width: double.infinity,
+                  height: 80.0,
+                  padding: const EdgeInsetsDirectional.fromSTEB(24.0, 0.0, 24.0, 0.0),
+                  iconPadding:
+                      const EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 0.0, 0.0),
+                  color: FlutterFlowTheme.of(context).primary,
+                  textStyle: FlutterFlowTheme.of(context).titleSmall.override(
+                        fontFamily: 'Readex Pro',
+                        color: Colors.white,
+                        letterSpacing: 0.0,
+                      ),
+                  elevation: 3.0,
+                  borderSide: const BorderSide(
+                    color: Colors.transparent,
+                    width: 1.0,
+                  ),
+                  borderRadius: BorderRadius.circular(8.0),
                 ),
-                borderRadius: BorderRadius.circular(8.0),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
